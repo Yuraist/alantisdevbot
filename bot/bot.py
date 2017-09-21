ff72c9de15868f800dbc5985e6ab0b6e613b1815
Yuri Istomin <yura.ist@icloud.com>
2017-09-22 00:03:17 +0300

    Created a message handler that echos the message

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,22 @@
 import os
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 
 def start(bot, update):
     update.message.reply_text('Hello World!')
 
+
 def hello(bot, update):
     user = update.message.from_user
     update.message.reply_text('Hello {}'.format(user.first_name))
+
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 token = os.environ['TOKEN']
 updater = Updater(token=token)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
