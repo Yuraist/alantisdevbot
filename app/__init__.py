6c27db51f99d19edea67092fa616ee0b042103ed
Yuri Istomin <yura.ist@icloud.com>
2017-09-21 23:56:27 +0300

    Changed run app

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+from bot.bot import updater
+
+updater.start_polling()
+updater.idle()
+
+@app.route('/')
+def index():
+    return 'Hello, I am @AlantisDevBot. Talk with me in the Telegram app.'
--- app/app.py
+++ app/app.py
@@ -1,18 +1,1 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-
-from bot.bot import updater
-
-updater.start_polling()
-updater.idle()
-
-@app.route('/')
-def index():
-    return 'Hello, I am @AlantisDevBot. Talk with me in the Telegram app.'
-
-
-if __name__ == '__main__':
-    app.run()
